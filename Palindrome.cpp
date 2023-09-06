eb5cbdbd859152ff11e645db4f08fe1f3735027d
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-09-06 19:15:12 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,2 +1,33 @@
 1. check number is palindrome or not
+soln - 
+  //@pb7
+#include <iostream>
+
+using namespace std;
+
+int reverse(int num){
+    int sum=0,r;
+    while(num>0){
+       r = num %10;
+       sum = sum*10 + r;
+       num=num/10;
+    }
+    
+    return sum;
+    
+}
+
+int main()
+{
+    int num = 124;
+    int x = num;
+    int revNum = reverse(num);
+    if(revNum == x){
+        cout<<"palindrome"<<endl;
+    }else{
+        cout<<"not palindrome"<<endl;
+    }
+
+    return 0;
+}
 
