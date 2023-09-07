a832696f87c1b024c7e9fc37ea756e18cb0b537f
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-09-07 22:47:01 +0530

    Update prime_no.cpp

--- prime_no.cpp
+++ prime_no.cpp
@@ -1,1 +1,34 @@
 q3. prime no 
+ans . //@pb7
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool prime (int num)
+{
+    if(num==0 || num==1) return false;
+  for (int i = 2; i <= sqrt(num); i++)
+    {
+      if (num % i == 0)
+	{
+	  return false;
+	  
+	}
+    }
+    
+  return true;
+
+}
+
+int main ()
+{
+  int num = 9;
+  bool ans = prime(num);
+
+  if ( ans == true)
+    {
+      cout << "prime" << endl;
+    }
+  else{
+    cout<<"not prime"<<endl;
+  }
+}
